14c99af75c74371ff7a09b85a1d50b682811bbc6
EchoFUN <xukai.ken@gmail.com>
2013-02-04 17:39:32 +0800

    dev

--- work/general.js
+++ work/general.js
@@ -4,24 +4,46 @@
  *
  */
 
-/* DOM操作模型 */
-var food = $('food'), mouse = $('mouse'), eye = $('eye');
-var foodStyle = food.style;
-food.addEvent('click', function() {
+;(function() {
 
-});
+	/* DOM操作模型 */
+	var food = $('food'), mouse = $('mouse'), eye = $('eye');
+	food.addEvent('click', function() {
 
-/* 算法模型 */
-var latency = 500, timer = 0, flag = 1, left = parseInt(foodStyle.left), top = parseInt(foodStyle.top);
+	});
 
-var forwardHook = function(times) {
-	return !!(times / 10 % 2 < 1);
-}, easeHook = function() {
-	return !!((flag *= -1) < 0);
-};
+	/* 算法模型 */
+	var latency = 500, timer = 0, flag = 1, left = parseInt(food.getStyle('left')), top = parseInt(food.getStyle('top')), delta = 50;
 
-var horizontalTimer = setInterval(function() {
-	
-}, latency), verticalTimer = setInterval(function() {
+	var forwardHook = function(times) {
+		return !!(times / 10 % 2 < 1);
+	}, easeHook = function() {
+		return !!((flag *= -1) < 0);
+	};
 
-}, latency);
+	(function(s, e) {
+		if (e <= s) {
+			this.style.top = (s--) + 'px';
+
+			var self = this, callee = arguments.callee;
+			setTimeout(function() {
+				callee.call(self, s, e);
+			}, 50);
+		} else {
+			food['horiMove'] = arguments.callee;
+		}
+	}).call(food, top, top + 50);
+	top += 50;
+
+	var horizontalTimer = setInterval(function() {
+		if (parseInt(food.getStyle('top')) > top)
+			return;
+
+		food.horiMove(top, top - 50);
+		top -= 50;
+	}, latency), verticalTimer = setInterval(function() {
+		;
+	}, latency);
+
+})();
+
